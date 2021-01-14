75f8b0e61a198e88186dc8eedfa022d0a6de6881
Germain Perdigal <perdigalgermain@gmail.com>
2021-01-14 15:43:42 +0100

    feat/landing-page

--- src/app/auth/components/landing/landing.component.ts
+++ src/app/auth/components/landing/landing.component.ts
@@ -8,6 +8,24 @@ import {AuthService} from '../../../core/services/auth.service';
 })
 export class LandingComponent implements OnInit {
 
+  avantages: { title: string, icon: string, content: string }[] = [
+    {
+      title: 'Une grande communauté',
+      icon: 'fas fa-users',
+      content: 'Rejoignez une communauté de plus de 100 utilisateurs !',
+    },
+    {
+      title: 'Une variété de centres d\'intérêts',
+      icon: 'fas fa-handshake',
+      content: 'Vous trouverez à coup sur vos centres d\'intérêts ici !',
+    },
+    {
+      title: 'Et c\'est gratuit',
+      icon: 'fas fa-euro-sign',
+      content: 'Pourquoi payer pour rencontrer du monde ?',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
