9c188721bc745096610917ab49526d93884e4c51
Germain Perdigal <perdigalgermain@gmail.com>
2021-01-14 16:55:29 +0100

    feat/display-tags-on-admin (WIP)

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {AdminRoutingModule} from './admin-routing.module';
 import {SharedModule} from '../shared/shared.module';
-
-
+import {UserPartsModule} from '../shared/user-parts/user-parts.module';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    SharedModule
+    SharedModule,
+    MatTableModule,
+    UserPartsModule
   ]
 })
 export class AdminModule { }
--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {TagService} from '../../core/services/tag.service';
+import {Observable} from 'rxjs';
+import {TagInterface} from '../../core/interfaces/tag.interface';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'label', 'iteration'];
+  tags$: Observable<TagInterface[]>;
 
-  ngOnInit() {
+  constructor(private tagService: TagService) {
+    this.tags$ = this.tagService.getAllTags();
   }
 
 }
